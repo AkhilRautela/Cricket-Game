3d407f3383b3d7a8f8eb6787858d1e0afaee9e5b
AkhilRautela <akhilrautela36@gmail.com>
2022-03-07 16:24:19 +0530

    Added Class Based Response in Getting Match Result

--- src/main/java/com/cricketgame/controllers/MatchController.java
+++ src/main/java/com/cricketgame/controllers/MatchController.java
@@ -2,6 +2,7 @@ package com.cricketgame.controllers;
 
 
 import com.cricketgame.models.Inning;
+import com.cricketgame.models.Player;
 import com.cricketgame.models.Team;
 import com.cricketgame.repositories.MatchRepository;
 import com.cricketgame.repositories.TeamRepository;
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,30 +25,12 @@ import java.util.Map;
 public class MatchController {
 
     @RequestMapping("/matchdetails/{id}")
-    public Map<String,String> getMatchDetails(@PathVariable(value = "id") int id) throws SQLException {
+    public MatchDetails getMatchDetails(@PathVariable(value = "id") int id) throws SQLException {
         DataFetchService dataFetchService = new DataFetchService();
         dataFetchService.getMatch(id);
         Inning inning1 = dataFetchService.getInning1();
         Inning inning2 = dataFetchService.getInning2();
         return fillMatchDetails(inning1,inning2,id);
-    }
-
-    private Map<String, String> fillMatchDetails(Inning inning1, Inning inning2, int matchId) {
-        Map <String, String> response = new HashMap<String, String>();
-        try {
-            response.put("status","1");
-            response.put("matchId", String.valueOf(matchId));
-            response.put("team1", inning1.getBattingTeam().getName().toString());
-            response.put("team2", inning1.getBowlingTeam().getName().toString());
-            response.put("team1score", String.valueOf(MatchUtils.getScore(inning1)));
-            response.put("team2score", String.valueOf(MatchUtils.getScore(inning2)));
-            response.put("team1wickets", String.valueOf(MatchUtils.getTotalWickets(inning2)));
-            response.put("team2wickets", String.valueOf(MatchUtils.getTotalWickets(inning1)));
-        }
-        catch (Exception e){
-            response.put("status", "0");
-        }
-        return response;
     }
 
     @RequestMapping("/startMatch/{team1Id}/{team2Id}/{overs}")
@@ -68,4 +53,135 @@ public class MatchController {
         return response;
     }
 
+    private MatchDetails fillMatchDetails(Inning inning1, Inning inning2, int matchId) {
+        MatchDetails matchDetails = new MatchDetails();
+        matchDetails.matchId = matchId;
+        matchDetails.teamDetails = new ArrayList<TeamDetails>();
+        try {
+            Team Team1 = inning1.getBattingTeam();
+            Team Team2 = inning1.getBowlingTeam();
+            matchDetails.teamDetails.add(getTeamDetails(Team1,inning1,inning2));
+            matchDetails.teamDetails.add(getTeamDetails(Team2,inning2,inning1));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return matchDetails;
+    }
+
+    private TeamDetails getTeamDetails(Team team, Inning inning1, Inning inning2) {
+
+        TeamDetails teamDetails = new TeamDetails();
+        teamDetails.setTeamName(team.getName().toString());
+        teamDetails.setTotalScore(MatchUtils.getScore(inning1));
+        teamDetails.setTotalWicketTaken(MatchUtils.getTotalWickets(inning2));
+        teamDetails.setPlayerDetails(new ArrayList<PlayerDetails>());
+
+        for(Player player : team.getPlayers()){
+            teamDetails.playerDetails.add(getPlayerDetails(player,inning1,inning2));
+        }
+        return teamDetails;
+    }
+
+    private PlayerDetails getPlayerDetails(Player player, Inning inning1, Inning inning2) {
+        PlayerDetails playerdetail = new PlayerDetails();
+        playerdetail.setName(player.getName());
+        playerdetail.setScoreInTheMatch(MatchUtils.getScoreOfPlayer(inning1,player));
+        playerdetail.setWicketTakenInTheMatch(MatchUtils.getWicketsTakenByPlayer(inning2,player));
+        return playerdetail;
+    }
+
+
+    class PlayerDetails{
+        String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getScoreInTheMatch() {
+            return scoreInTheMatch;
+        }
+
+        public void setScoreInTheMatch(int scoreInTheMatch) {
+            this.scoreInTheMatch = scoreInTheMatch;
+        }
+
+        public int getWicketTakenInTheMatch() {
+            return wicketTakenInTheMatch;
+        }
+
+        public void setWicketTakenInTheMatch(int wicketTakenInTheMatch) {
+            this.wicketTakenInTheMatch = wicketTakenInTheMatch;
+        }
+
+        int scoreInTheMatch;
+        int wicketTakenInTheMatch;
+    }
+
+    class TeamDetails{
+        String teamName;
+        int totalScore;
+        int totalWicketTaken;
+
+        public String getTeamName() {
+            return teamName;
+        }
+
+        public void setTeamName(String teamName) {
+            this.teamName = teamName;
+        }
+
+        public int getTotalScore() {
+            return totalScore;
+        }
+
+        public void setTotalScore(int totalScore) {
+            this.totalScore = totalScore;
+        }
+
+        public int getTotalWicketTaken() {
+            return totalWicketTaken;
+        }
+
+        public void setTotalWicketTaken(int totalWicketTaken) {
+            this.totalWicketTaken = totalWicketTaken;
+        }
+
+        public ArrayList<PlayerDetails> getPlayerDetails() {
+            return playerDetails;
+        }
+
+        public void setPlayerDetails(ArrayList<PlayerDetails> playerDetails) {
+            this.playerDetails = playerDetails;
+        }
+
+        ArrayList <PlayerDetails> playerDetails;
+    }
+
+    class MatchDetails{
+        int matchId;
+
+        public int getMatchId() {
+            return matchId;
+        }
+
+        public void setMatchId(int matchId) {
+            this.matchId = matchId;
+        }
+
+        public ArrayList<TeamDetails> getTeamDetails() {
+            return teamDetails;
+        }
+
+        public void setTeamDetails(ArrayList<TeamDetails> teamDetails) {
+            this.teamDetails = teamDetails;
+        }
+
+        ArrayList <TeamDetails> teamDetails;
+    }
 }
